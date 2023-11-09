02b786710d33557c9687b876bdaacdf889174181
vishal <vishalmishra6604@gmail.com>
2023-11-09 21:51:30 +0530

    feat: all curd operations

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 8000;
-
+const route = express.Router();
 // connect data-base
 mongoose.connect("mongodb://0.0.0.0:27017/users").then(() => console.log("Mongo Connected...")).catch((err) => console.log("mongo Error: ", err))
 
-const enums = {
-    type: String,
-    required: true,
-    unique: true,
-}
 // schema
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -21,7 +16,9 @@ const userSchema = new mongoose.Schema({
         type: String,
     },
     email: {
-        enums
+        type: String,
+        required: true,
+        unique: true,
     },
     jobTitle: {
         type: String,
@@ -40,7 +37,7 @@ app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
 // routes
-app.get("/api/users", async (req, res) => {
+app.post("/api/users", async (req, res) => {
     const body = req.body;
     if (!body || !body.firstName || !body.lastName || !body.email || !body.gender || !body.jobTitle) {
 
@@ -53,8 +50,48 @@ app.get("/api/users", async (req, res) => {
         gender: body.gender,
         jobTitle: body.jobTitle,
     });
-    console.log("result :", result);
-    return res.status(201).json({msg:"success"});
+    return res.status(201).json({ msg: "success" });
+})
+app.route("/api/user/:_id").get(async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const user = await User.findOne({ _id })
+        if (user) {
+            res.json(user);
+        }
+        else {
+            res.status(404).json({ msg: "User not found" });
+        }
+    } catch (error) {
+        console.log("Get User Error >>", error);
+    }
 
-    
+}).patch(async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const user = await User.findById({ _id });
+        if (!user) {
+            return res.status(404).json({ msg: 'User not found' });
+        }
+        Object.assign(user, req.body);
+        const updatedUser = await user.save();
+        res.json(updatedUser);
+
+    } catch (error) {
+        console.log("Patch User Error >>", error);
+        return res.status(404).json({ msg: error.message });
+    }
+}).delete(async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const deletedUser = await User.findByIdAndDelete({ _id });
+        if (deletedUser) {
+            res.json({ msg: 'User deleted successfully' });
+        } else {
+            res.status(404).json({ msg: 'User not found' });
+        }
+    } catch (error) {
+        console.log("Delete User Error >>", error);
+        return res.status(404).json({ msg: error.message });
+    }
 })
